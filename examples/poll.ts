ca2838a0a23ad5466d74545c66e544a6b755ebbe
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2025-10-02 11:20:17 -0400

    fix: clean up examples

--- examples/poll.ts
+++ examples/poll.ts
@@ -1,6 +1,6 @@
 import { config as dotenvConfig } from "dotenv";
 import { resolve } from "path";
-import { AuthArgs, RelayerTransactionState } from "../src/types";
+import { RelayerTransactionState } from "../src/types";
 import { RelayClient } from "../src/client";
 
 dotenvConfig({ path: resolve(__dirname, "../.env") });
@@ -12,11 +12,7 @@ async function main() {
     
     const relayerUrl = `${process.env.RELAYER_URL}`;
     const chainId = parseInt(`${process.env.CHAIN_ID}`);
-    const args: AuthArgs = {
-        authUrl: `${process.env.AUTH_URL}`,
-        authToken: `${process.env.AUTH_TOKEN}`,
-    };
-    const client = new RelayClient(relayerUrl, chainId, undefined, args);
+    const client = new RelayClient(relayerUrl, chainId);
 
     // const states = [RelayerTransactionState.STATE_EXECUTED.valueOf(), RelayerTransactionState.STATE_CONFIRMED.valueOf()];
     const states = [RelayerTransactionState.STATE_CONFIRMED.valueOf()];
